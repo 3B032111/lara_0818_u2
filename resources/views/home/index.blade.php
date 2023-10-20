2b9f98256c614e0c30f428e65a8414b39e744d23
3B032111 <3b032111@gm.student.ncut.edu.tw>
2023-10-21 02:59:22 +0800

    git practice2 add master.blade.php and extends master

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -6,5 +6,11 @@
 </head>
 <body>
 lara_0818_u2 test 2
+@extends('layouts.master')
+@section('title','Home')
+@section('content')
+    <h1>Home</h1>
+    <p>This is my body content.</p>
+@endsection
 </body>
 </html>
